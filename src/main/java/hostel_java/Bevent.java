0ef4f1efed7cd597b19b515d9e6d7c798af80e98
Suja <Suja.mbidri@gmail.com>
2020-05-14 23:48:54 +0530

    established Bevent connection

--- src/main/java/hostel_java/Bevent.java
+++ src/main/java/hostel_java/Bevent.java
@@ -1,4 +1,5 @@
  package hostel_java;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 
@@ -9,23 +10,37 @@ public class Bevent {
          JFrame frame= new JFrame("BOYS HOSTEL");
        
         frame.setSize(500,400);
+        frame.setSize(500,400);
         JPanel panel=new JPanel();  
         panel.setLayout(null);
         frame.add(panel);
         JButton button=new JButton("MESS");
         button.setBounds(170,100,150,50);
         panel.add(button);
+
+       
         
-        
+
         JButton button2=new JButton("LAUNDRY");
         button2.setBounds(170,170,150,50);
         panel.add(button2);
-//        button2.addActionListener (new First.Action2());
-//        button.addActionListener (new First.Action1());
         JButton button3=new JButton("STUDENT INFO");
         button3.setBounds(170,240,150,50);
+        
+        button3.addActionListener((ActionEvent e) -> {
+             new infoB();
+             
+             
+         }
+         );
+        
+        
+        
+    
         panel.add(button3);
         frame.setVisible(true);
     }
+
+    
     
 }
